0c9dc532db2daaa68cdefc1cbe91647d9fe67a6a
Suriyarithik <suriyavedsum@gmail.com>
2017-07-03 09:31:54 +0530

    Create Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+# Helloworld
+import java.io.*;
+import java.util.*;
+public class Helloworld
+{ 
+  public static void main(String args[[])throws IOException
+   {
+     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+     String n=br.readLine();
+     for(i=0;i<=n;i++)
+      {
+        System.out.println("Hello");
+      }
+    }
+ }   
